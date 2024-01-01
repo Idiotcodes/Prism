70308c173b50f6b9fd07fa5fca26bba39de594fe
BearCodes <113568440+bear-codes@users.noreply.github.com>
2024-01-01 16:27:04 +0530

    Update error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,9 +3,14 @@ import React from "react";
 import ErrorImage from "@/assets/error.gif";
 import Image from "next/image";
 import Link from "next/link";
+import Head from 'next/head';
 
 function error({ error, reset }: any) {
   return (
+    <>
+      <Head>
+        <script async src="https://arc.io/widget.min.js#gM16djwH"></script>
+      </Head>
     <div className="fixed w-screen h-screen top-0 bg-base-300">
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col space-y-5 items-center justify-center">
         <Image
@@ -29,6 +34,7 @@ function error({ error, reset }: any) {
         </div>
       </div>
     </div>
+    <>
   );
 }
 
