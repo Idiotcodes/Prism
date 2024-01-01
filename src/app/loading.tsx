b68534e26b29a4f32f3caa5afce3afe8365ac0da
BearCodes <113568440+bear-codes@users.noreply.github.com>
2024-01-01 16:27:46 +0530

    Update loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import LoadingGIF from "@/assets/genkai.gif";
+import Head from 'next/head';
 
 function Loading() {
   return (
+    <>
+      <Head>
+        <script async src="https://arc.io/widget.min.js#gM16djwH"></script>
+      </Head>
     <div className="fixed w-screen h-screen top-0 bg-base-300 opacity-70">
       <img
         src={LoadingGIF.src}
@@ -12,6 +17,7 @@ function Loading() {
         className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
       />
     </div>
+    </>
   );
 }
 
