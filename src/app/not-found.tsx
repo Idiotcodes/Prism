8b860b380044b7c434157ee867d0d9c9030d2d03
BearCodes <113568440+bear-codes@users.noreply.github.com>
2024-01-01 16:28:46 +0530

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Head from 'next/head';
 
 function not_found() {
   return (
+    <>
+      <Head>
+        <script async src="https://arc.io/widget.min.js#gM16djwH"></script>
+      </Head>
     <div className="flex justify-center items-center h-full">
       <div>Page Not Found</div>
     </div>
+    </>
   );
 }
 
