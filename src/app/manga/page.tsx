38e10d4048547138ce5ca2fc692dc9127eea38f1
BearCodes <113568440+bear-codes@users.noreply.github.com>
2024-01-01 16:26:08 +0530

    Update page.tsx

--- src/app/manga/page.tsx
+++ src/app/manga/page.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import Head from 'next/head';
 
 function page() {
   return (
+    <>
+      <Head>
+        <script async src="https://arc.io/widget.min.js#gM16djwH"></script>
+      </Head>
     <div className="m-5 lg:m-10">
       <div className="h-20"></div>
       Coming Soon!
     </div>
+    </>
   );
 }
 
