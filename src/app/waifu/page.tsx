e1339ff00ac7d26a3daa7401cbbc21a24f8e1b22
BearCodes <113568440+bear-codes@users.noreply.github.com>
2024-01-01 16:22:34 +0530

    Update page.tsx

--- src/app/waifu/page.tsx
+++ src/app/waifu/page.tsx
@@ -1,6 +1,7 @@
 import { TWaifuCategory } from "@/@types/WaifuCategory";
 import useWaifu from "@/hooks/useWaifu";
 import KitsuneMasonry from "./KitsuneMasonry";
+import Head from 'next/head';
 
 async function page() {
   const { getManyWaifu } = useWaifu();
@@ -8,9 +9,14 @@ async function page() {
   const waifuInfo = await getManyWaifu(category);
 
   return (
+    <>
+      <Head>
+        <script async src="https://arc.io/widget.min.js#gM16djwH"></script>
+      </Head>
     <div className="m-2">
       <KitsuneMasonry waifuInfo={waifuInfo} />
     </div>
+    </>
   );
 }
 
